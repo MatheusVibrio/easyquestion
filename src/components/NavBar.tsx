0a3307f638d486b93679f79dc9fdaa55dab00460
unknown <Android@TERM5-06.LABFAE.local>
2024-04-29 20:01:47 -0300

    feat :: Nova SideBar e NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,107 +10,58 @@ import {
   
   export default function NavBar() {
     return (
-    <main className="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
-        <nav className="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
-        <div className="flex items-center justify-between w-full px-4 py-1 mx-auto ml-[16rem] flex-wrap-inherit">
-          <div className="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
-            <div className="flex items-center md:ml-auto md:pr-4">
-              <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
-                <span className="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
-                  <i className="fas fa-search"></i>
-                </span>
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start rtl:justify-end">
+            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span className="sr-only">Open sidebar</span>
+                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                   <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                </svg>
+             </button>
+            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+          </div>
+          <div className="flex items-center">
+              <div className="flex items-center ms-3">
+                <div>
+                  <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                    <span className="sr-only">Open user menu</span>
+                    <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
+                  </button>
+                </div>
+                <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                  <div className="px-4 py-3" role="none">
+                    <p className="text-sm text-gray-900 dark:text-white" role="none">
+                      Neil Sims
+                    </p>
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                      neil.sims@flowbite.com
+                    </p>
+                  </div>
+                  <ul className="py-1" role="none">
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <ul className="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
-              
-              <li className="flex items-center">
-                <a href="../pages/sign-in.html" className="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand">
-                  <i className="fa fa-user sm:mr-1"></i>
-                  <span className="hidden sm:inline">Perfil</span>
-                </a>
-              </li>
-              <li className="flex items-center pl-4 xl:hidden">
-                <a href="javascript:;" className="block p-0 text-sm text-white transition-all ease-nav-brand" sidenav-trigger>
-                  <div className="w-4.5 overflow-hidden">
-                    <i className="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"></i>
-                    <i className="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"></i>
-                    <i className="ease relative block h-0.5 rounded-sm bg-white transition-all"></i>
-                  </div>
-                </a>
-              </li>
-              <li className="flex items-center px-4">
-                <a href="javascript:;" className="p-0 text-sm text-white transition-all ease-nav-brand">
-                  <i fixed-plugin-button-nav className="cursor-pointer fa fa-cog"></i>
-                 
-                </a>
-              </li>
-
-              
-
-              <li className="relative flex items-center pr-2">
-                <p className="hidden transform-dropdown-show"></p>
-                <a href="javascript:;" className="block p-0 text-sm text-white transition-all ease-nav-brand" dropdown-trigger aria-expanded="false">
-                  <i className="cursor-pointer fa fa-bell"></i>
-                </a>
-
-                <ul dropdown-menu className="text-sm transform-dropdown before:font-awesome before:leading-default before:duration-350 before:ease lg:shadow-3xl duration-250 min-w-44 before:sm:right-8 before:text-5.5 pointer-events-none absolute right-0 top-0 z-50 origin-top list-none rounded-lg border-0 border-solid border-transparent dark:shadow-dark-xl dark:bg-slate-850 bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all before:absolute before:right-2 before:left-auto before:top-0 before:z-50 before:inline-block before:font-normal before:text-white before:antialiased before:transition-all before:content-['\f0d8'] sm:-mr-6 lg:absolute lg:right-0 lg:left-auto lg:mt-2 lg:block lg:cursor-pointer">
-                  <li className="relative mb-2">
-                    <a className="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg bg-transparent px-4 duration-300 hover:bg-gray-200 hover:text-slate-700 lg:transition-colors" href="javascript:;">
-                      <div className="flex py-1">
-                        <div className="my-auto">
-                          <img src="../assets/img/team-2.jpg" className="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
-                        </div>
-                        <div className="flex flex-col justify-center">
-                          <h6 className="mb-1 text-sm font-normal leading-normal dark:text-white"><span className="font-semibold">New message</span> from Laur</h6>
-                          <p className="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                            <i className="mr-1 fa fa-clock"></i>
-                            13 minutes ago
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-
-                  <li className="relative mb-2">
-                    <a className="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg px-4 transition-colors duration-300 hover:bg-gray-200 hover:text-slate-700" href="javascript:;">
-                      <div className="flex py-1">
-                        <div className="my-auto">
-                          <img src="../assets/img/small-logos/logo-spotify.svg" className="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
-                        </div>
-                        <div className="flex flex-col justify-center">
-                          <h6 className="mb-1 text-sm font-normal leading-normal dark:text-white"><span className="font-semibold">New album</span> by Travis Scott</h6>
-                          <p className="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                            <i className="mr-1 fa fa-clock"></i>
-                            1 day
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-
-                  <li className="relative">
-                    <a className="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg px-4 transition-colors duration-300 hover:bg-gray-200 hover:text-slate-700" href="javascript:;">
-                      <div className="flex py-1">
-                        <div className="inline-flex items-center justify-center my-auto mr-4 text-sm text-white transition-all duration-200 ease-nav-brand bg-gradient-to-tl from-slate-600 to-slate-300 h-9 w-9 rounded-xl">
-                          
-                        </div>
-                        <div className="flex flex-col justify-center">
-                          <h6 className="mb-1 text-sm font-normal leading-normal dark:text-white">Payment successfully completed</h6>
-                          <p className="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                            <i className="mr-1 fa fa-clock"></i>
-                            2 days
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
         </div>
-      </nav>
-      </main>
+      </div>
+    </nav>
     );
   }
   
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,14 @@
+import Dashboard from "../../components/Dashboard";
 import Intro from "../../components/Intro";
+import NavBar from "../../components/NavBar";
+import SideBar from "../../components/SideBar";
 
 export default function Home() {
   return (
     <main>
-      <Intro/>
+      <SideBar />
+      <NavBar />
+      <Dashboard />
     </main>
   )
 }
